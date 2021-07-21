b95156f717093d5c87fc94889626c414f2f46cef
njorquera98 <nicolas.jorquera98@gmail.com>
2021-07-21 17:58:11 -0400

    First commit

---
+++ src/pages/Sign-in.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-pa-md">
+    <div class="q-gutter-md" style="max-width: 300px">     
+      <q-input outlined v-model="text" label="User Name" />
+      <q-input outlined v-model="text" label="Password1" />
+    </div>
+  </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { ref } from 'vue';
+
+
+export default defineComponent({
+  name: 'PageIndex',
+
+  setup () {
+    return {
+      text: ref('')
+    }
+  }
+  
+})
+</script>
